b8ec3c8756a675de47796793541e3b2b1cde7b84
SebastianDarie <s.d.01.2002@gmail.com>
2021-08-08 23:09:22 +0300

    almost done with projects

--- src/components/ProjectPreview/index.tsx
+++ src/components/ProjectPreview/index.tsx
@@ -10,32 +10,44 @@ interface Project {
   img: string;
   description: string;
   tags: string[];
+  live_url: string;
 }
 
 const PortfolioPreview: React.FC<{ project: Project }> = ({ project }) => {
   return (
-    <div className={styles.card}>
-      <div
-        className={styles.titleCard}
-        style={{ backgroundImage: `url(${project.img})` }}
-      >
-        {/* <h1 className={styles.title}>{project.title}</h1> */}
-      </div>
-      <div className='pa3'>
-        <p className={`${styles.desc} mt0 mb2`}>{project.description}</p>
-        <div className={styles.tags}>
-          Tagged:
-          {project.tags.map((t) => (
-            <div className={styles.tag} data-tag={t}>
-              {t}
-            </div>
-          ))}
-        </div>
-        <a className={styles.link} href={project.url}>
-          <span className={styles.linkInner}>View</span>
+    <>
+      <a href={project.live_url} target='_blank' className={styles.projectLink}>
+        <h2>{project.title}</h2>
+      </a>
+      {/* <article className={styles.article}>
+        <a href={project.live_url} target='_blank'>
+          <img src={project.img} className={styles.projectImg} />
         </a>
+        <a href='' target='_blank'></a>
+        <a href='' target='_blank'></a>
+        <p></p>
+      </article> */}
+      <div className={styles.card}>
+        <div
+          className={styles.titleCard}
+          style={{ backgroundImage: `url(${project.img})` }}
+        ></div>
+        <div className='pa3'>
+          <p className={`${styles.desc} mt0 mb2`}>{project.description}</p>
+          <div className={styles.tags}>
+            Highlights:
+            {project.tags.map((t) => (
+              <div className={styles.tag} data-tag={t}>
+                {t}
+              </div>
+            ))}
+          </div>
+          <a className={styles.link} href={project.url}>
+            <span className={styles.linkInner}>View</span>
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/project/airbnb-clone.md
+++ src/pages/project/airbnb-clone.md
@@ -1,17 +1,17 @@
 ---
 layout: ../../layouts/project.astro
-title: Twitter Clone
+title: Airbnb Clone
 client: Self
-published_at: 2021-08-07 00:00:00
-img: /assets/twitter-clone.png
+published_at: 2021-08-05 00:00:00
+img: /assets/airbnb-clone.png
 description: |
-  React App which uses Firebase as a BaaS to power all of it's CRUD functionality.
+  Next.js PWA with real time geosearch through Google Maps and payments with Stripe.
 tags:
-  - react
-  - redux
-  - firebase
-  - styled-components
-live_url: https://twitter-clone-c0b2e.web.app/
+  - next.js
+  - google maps
+  - postgis
+  - sass
+live_url: https://mernlabs.team/
 ---
 
 <!-- This checkout page was made and designed for Precision Reloading with the intentions to bring them to a more modern stack. This single page application is built with Nuxt.js and Express on an IBM iSeries. The backend was integrated with their existing business suite written with RPG. This was very challenging due to the difficulty of working with the IBM iSeries as a web developer. I did all the dev-ops to get this running live, as well as designed the frontend and backend architecture. If you would like to login, you can use "register7@softaltern.com" for the username and "testtest" for the password. -->
